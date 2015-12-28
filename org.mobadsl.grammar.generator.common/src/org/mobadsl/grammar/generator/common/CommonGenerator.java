efd5b8429bccf8b3c25466854b58e613ffede920
Florian Pirchner <florian.pirchner@gmail.com>
2015-12-28 11:05:48 +0100

    Added sample

--- org.mobadsl.grammar.generator.common/src/org/mobadsl/grammar/generator/common/CommonGenerator.java
+++ org.mobadsl.grammar.generator.common/src/org/mobadsl/grammar/generator/common/CommonGenerator.java
@@ -7,36 +7,38 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGeneratorContext;
 import org.eclipse.xtext.generator.OutputConfiguration;
-import org.mobadsl.grammar.generator.common.IGeneratorDelegate;
-import org.mobadsl.semantic.model.moba.MobaApplication;
-import org.mobadsl.semantic.model.moba.MobaConstant;
+import org.mobadsl.semantic.model.moba.MobaModel;
 
 public class CommonGenerator implements IGeneratorDelegate {
 
+	@SuppressWarnings("unused")
 	@Override
 	public void beforeGenerate(Resource input, IFileSystemAccess2 fsa, IGeneratorContext context,
 			PropertiesMap properties) {
-
+		MobaModel model = (MobaModel) input.getContents().get(0);
 	}
 
+	@SuppressWarnings("unused")
 	@Override
 	public void afterGenerate(Resource input, IFileSystemAccess2 fsa, IGeneratorContext context,
 			PropertiesMap properties) {
-
+		MobaModel model = (MobaModel) input.getContents().get(0);
 	}
 
+	@SuppressWarnings("unused")
 	@Override
 	public void generate(Resource input, IFileSystemAccess2 fsa, IGeneratorContext context, PropertiesMap properties) {
-		fsa.generateFile("Foo", "server-rest", "Heyho");
+		MobaModel model = (MobaModel) input.getContents().get(0);
 		
+		fsa.generateFile("Foo", "mobile", "Heyho");
 	}
 
 	@Override
 	public Set<OutputConfiguration> getOutputConfigurations() {
-		
-		OutputConfiguration config = new OutputConfiguration("server-rest");
-		config.setDescription("Generates REST services for the server side.");
-		config.setOutputDirectory("./rest");
+
+		OutputConfiguration config = new OutputConfiguration("mobile");
+		config.setDescription("Generates Mobile stuff.");
+		config.setOutputDirectory("./mobile");
 
 		return Collections.singleton(config);
 	}
--- org.mobadsl.grammar.generator.server.rest/src/org/mobadsl/grammar/generator/server/rest/RestServicesGenerator.java
+++ org.mobadsl.grammar.generator.server.rest/src/org/mobadsl/grammar/generator/server/rest/RestServicesGenerator.java
@@ -8,32 +8,34 @@ import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGeneratorContext;
 import org.eclipse.xtext.generator.OutputConfiguration;
 import org.mobadsl.grammar.generator.common.IGeneratorDelegate;
-import org.mobadsl.semantic.model.moba.MobaApplication;
-import org.mobadsl.semantic.model.moba.MobaConstant;
+import org.mobadsl.semantic.model.moba.MobaModel;
 
 public class RestServicesGenerator implements IGeneratorDelegate {
 
+	@SuppressWarnings("unused")
 	@Override
 	public void beforeGenerate(Resource input, IFileSystemAccess2 fsa, IGeneratorContext context,
 			PropertiesMap properties) {
-
+		MobaModel model = (MobaModel) input.getContents().get(0);
 	}
 
+	@SuppressWarnings("unused")
 	@Override
 	public void afterGenerate(Resource input, IFileSystemAccess2 fsa, IGeneratorContext context,
 			PropertiesMap properties) {
-
+		MobaModel model = (MobaModel) input.getContents().get(0);
 	}
 
+	@SuppressWarnings("unused")
 	@Override
 	public void generate(Resource input, IFileSystemAccess2 fsa, IGeneratorContext context, PropertiesMap properties) {
+		MobaModel model = (MobaModel) input.getContents().get(0);
 		fsa.generateFile("Foo", "server-rest", "Heyho");
-		
 	}
 
 	@Override
 	public Set<OutputConfiguration> getOutputConfigurations() {
-		
+
 		OutputConfiguration config = new OutputConfiguration("server-rest");
 		config.setDescription("Generates REST services for the server side.");
 		config.setOutputDirectory("./rest");
